b0397f32dfe037ace08d98fde784cbdfc4b4bb2e
maciekmalachowski <maciejmalachowski1@gmail.com>
2024-12-16 18:35:03 +0100

    Reading cv and calling it works fine

--- App/main.py
+++ App/main.py
@@ -1,4 +1,4 @@
-from taipy.gui import Gui
+from taipy.gui import Gui, State, notify
 import taipy.gui.builder as tgb
 from llama_index.llms.openai import OpenAI
 from scrapper import get_data
@@ -9,12 +9,35 @@ fselector = None
 file_path = None
 file_name = None
 ivalue = None
+offer_link = None
+correct_cv = None
+pdf_engine = None
+scrapped_data = None
+
+def call_read_cv(state: State):
+    try:
+        state.pdf_engine = read_cv(state.file_path)
+        print(state.pdf_engine)
+        notify(state, notification_type="success", message="CV successfully analyzed")
+        state.correct_cv = True
+    except:
+        state.correct_cv = False
+        notify(state, notification_type="error", message="Something is wrong with your CV")
+
+def call_get_data(state: State):
+    try:
+        state.scrapped_data = get_data(state.offer_link)
+        notify(state, notification_type="success", message="Job details are ready")
+    except:
+        notify(state, notification_type="error", message="Wrong link")
+
 
 # Function to update the file selector partial dynamically
 def on_change(state, name, val):
     if name == "fselector":
         fname = list(val.split("\\")[-1].split('.'))
         state.file_name = f"{fname[0]}.{fname[-1]}"
+        state.file_path = val
         with tgb.Page() as changed_page:
             tgb.file_selector(
                 content="{fselector}",
@@ -22,15 +45,17 @@ def on_change(state, name, val):
                 extensions=".pdf",
                 drop_message="Drop here to Upload",
                 class_name="fullwidth",
-                on_action=read_cv
+                notify=False,
+                on_action=call_read_cv
             )
-            tgb.part(partial="{url_input}")
+            if state.correct_cv:
+                tgb.part(partial="{url_input}")
         state.file_selector_partial.update_content(state, changed_page)
     
     if name == "ivalue":
-        activation = False if not val else True
+        state.offer_link = val
         with tgb.Page() as change_button:
-            tgb.button("Search for offer", on_action=get_data, class_name="fullwidth plain", active=activation)
+            tgb.button("Search for offer", on_action=call_get_data, class_name="fullwidth plain", active=bool(val))
         state.search_button.update_content(state, change_button)
 
     print(name, val)
@@ -49,8 +74,8 @@ with tgb.Page() as page:
 if __name__ == "__main__":
     gui = Gui(page)
     # Initialize partials
-    file_selector_partial = gui.add_partial("<|{fselector}|file_selector|label=Upload your CV|extensions=.pdf|drop_message=Drop here to Upload|class_name=fullwidth|on_action=read_cv|>")
+    file_selector_partial = gui.add_partial("<|{fselector}|file_selector|label=Upload your CV|extensions=.pdf|drop_message=Drop here to Upload|class_name=fullwidth|notify=False|on_action=call_read_cv|>")
     url_input = gui.add_partial("<|{ivalue}|input|label=Paste the url link to the job posting.|class_name=fullwidth|><|part|partial={search_button}|>")
-    search_button = gui.add_partial("<|Search for offer|button|on_action=read_cv|class_name=fullwidth plain|active=False|>")
+    search_button = gui.add_partial("<|Search for offer|button|class_name=fullwidth plain|active=False|>")
     # Run the GUI
     gui.run(title="🧙‍♂️CVWizard", use_reloader=True)
--- App/scrapper.py
+++ App/scrapper.py
@@ -1,29 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
-from deep_translator import ChatGptTranslator
-
 
 def get_data(link):
+    # Get response and init the BeautifulSoup
     response = requests.get(link)
     soup = BeautifulSoup(response.text, "html.parser")
-    # scrappe skills
+
+    # Scrappe skills
     skills =  [i.text for i in soup.find_all("h4", attrs={'class': 'css-b849nv'})]
 
-    # scrape description
+    # Scrape description
     full_desc = soup.find("div", attrs={'class': 'css-tbycqp'})
     sections = full_desc.find_all(['p', 'ul'])
-    desc = {}
-    current_key = None
+    desc = []
     for element in sections:
         if element.name == 'p':
             if element.get_text(strip=True):
-                current_key = element.get_text(strip=True)
-                desc[current_key] = []
-        elif element.name == 'ul' and current_key:
-            desc[current_key].extend([li.get_text(strip=True) for li in element.find_all('li')])
+                desc.append(element.get_text(strip=True))
+        elif element.name == 'ul':
+            for li in element.find_all('li'):
+                desc.append(li.get_text(strip=True))
 
-    result = {"skills":skills} | desc
-    print(result)
-    # return result
-
-get_data("https://justjoin.it/job-offer/nomachine-poland-c-c-porting-engineer-wroclaw-c")
+    result = {"skills":skills} | {"description": " ".join(desc)}
+    return result
